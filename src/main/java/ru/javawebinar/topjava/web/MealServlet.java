fc09eee489e3af9ff944ad835081dede7fcf3086
Vitaly Vedeshkin <isehead@gmail.com>
2020-06-10 10:47:56 +0500

    HW1 done: 1. External MealsGenerator created 2. Added getters to entities for requests from JSP 3. Added MealServlet 4. Added meals.jsp with forEach tag and DateTimeFormatter

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,14 +1,25 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsGenerator;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
@@ -17,9 +28,19 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+//        List<MealTo> mealToList = MealsUtil.filteredByStreams
+//                (MealsGenerator.generateHardcodedMeals(), LocalTime.MIN, LocalTime.MAX, 2000);
+//        List<MealTo> mealToList = Arrays.asList(
+//                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500, true),
+//                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000, false));
+        List<MealTo> mealToList = new ArrayList<>();
+        mealToList.add(new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500, true));
+        mealToList.add(new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000, false));
+
+        request.setAttribute("mealToList", mealToList);
+//        response.sendRedirect("meals");
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
     }
 
-    
+
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,38 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page import="java.time.format.DateTimeFormatter" %>
+
 <html lang="ru">
 <head>
-    <title>Users</title>
+    <title>Meals</title>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
-<h2>Users</h2>
+<h2>Meals</h2>
+
+<br><br>
+
+<table>
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th>Exceeded</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${mealToList}" var="mealTo">
+        <tr>
+            <td width="200">${mealTo.dateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))}</td>
+            <td width="200">${mealTo.description}</td>
+            <td width="200">${mealTo.calories}</td>
+            <td width="200">${mealTo.excess}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
