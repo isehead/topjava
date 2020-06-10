df1827d98571295ae79d62f88b5d1f229102b0bd
Vitaly Vedeshkin <isehead@gmail.com>
2020-06-10 14:31:17 +0500

    HW1: fixed after review #1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.MealTo;
-import ru.javawebinar.topjava.util.MealsGenerator;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
@@ -10,22 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Month;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    public static final int CALORIES_THRESHOLD = 2000;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
+        log.debug("get all meals");
 
-        List<MealTo> mealToList = MealsUtil.filteredByStreams(MealsGenerator.generate(), LocalTime.MIN, LocalTime.MAX, 2000);
+        List<MealTo> mealToList = MealsUtil.filteredByStreams(MealsUtil.meals, LocalTime.MIN, LocalTime.MAX, CALORIES_THRESHOLD);
         request.setAttribute("mealToList", mealToList);
         request.getRequestDispatcher("meals.jsp").forward(request, response);
     }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,7 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<%@ page import="java.time.format.DateTimeFormatter" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="javatime" uri="http://sargue.net/jsptags/time" %>
 
 <html lang="ru">
 <head>
@@ -13,23 +12,22 @@
 <h2>Meals</h2>
 
 <br><br>
-
 <table>
     <thead>
     <tr>
         <th>Date</th>
         <th>Description</th>
         <th>Calories</th>
-        <th>Exceeded</th>
     </tr>
     </thead>
     <tbody>
     <c:forEach items="${mealToList}" var="mealTo">
-        <tr style="${mealTo.excess == 'true' ? 'color: green' : 'color: red'}">
-            <td width="200">${mealTo.dateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))}</td>
+        <tr style="${mealTo.excess ? 'color: red' : 'color: green'}">
+            <td width="200">
+                <javatime:format value="${mealTo.dateTime}" pattern="yyyy-MM-dd HH:mm:ss" style="MS"/>
+            </td>
             <td width="200">${mealTo.description}</td>
             <td width="200">${mealTo.calories}</td>
-            <td width="200">${mealTo.excess}</td>
         </tr>
     </c:forEach>
     </tbody>
